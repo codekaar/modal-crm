07fd35d237a1eff400a1bba6d51fd433ec6b38fe
Emre <esokullu@gmail.com>
2019-08-07 11:50:11 -0700

    update

--- view/ticket_conversation.php
+++ view/ticket_conversation.php
@@ -40,6 +40,27 @@
                     ?>
                 </div>
                 <div>
+                    Recreate Url: <?= 
+                    
+                    "https://gr.ps/generate?" . http_build_query([
+                        "name" => $by->instances->first()->groups_name,
+                        "title" => $by->instances->first()->groups_title,
+                        "public_id" =>$by->instances->first()->uuid,
+                        "theme"=>$by->instances->first()->theme,
+                        "text_color"=>($by->instances->first()->theme == "dark" ) ? "white" :  'rgb(63, 95, 127)',
+                        "background_color"=> ($by->instances->first()->theme == "dark" ) ? 'rgb(71, 71, 71)' :  "white",
+                        "primary_color"=>$by->instances->first()->color,
+                        "host"=>"https://accounts.groups2.com",
+                        "secret"=>md5($by->instances->first()->groups_name.":burasi mustur yolu yokustur"),
+                        "regen"=>1,
+                        "git"=> "http://".$by->instances->first()->groups_name.":".substr(md5(password_hash($by->id, PASSWORD_BCRYPT, ["salt"=>"nuno gomes is a great soccer player"])), 3, 6) . (string) $by->id."@165.22.133.69/".$by->instances->first()->groups_name."/frontend",
+                        "description"=>$by->instances->first()->description
+                
+                    ])
+                    
+                    ?>
+                </div>
+                <div>
                     # of email conversation: <?= $by->service_conversations_count ?>
                 </div>
                 <div>
