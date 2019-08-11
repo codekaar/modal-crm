d8915e6d5af36693ff8f511f1c7664a85b0a4383
Emre <esokullu@gmail.com>
2019-08-10 18:41:29 -0700

    db entry

--- view/email/ticket_opened.html.php
+++ view/email/ticket_opened.html.php
@@ -8,8 +8,7 @@
 <body>
 
 <div>
-    <p>A ticket has been opened on your request.</p>
-    <p>Log in to https://graphjs.com or https://grou.ps to view your tickets.</p>
+    <?php nl2br($this->e($body)) ?>
 </div>
 
 </body>
