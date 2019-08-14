71ac697c4f356015fc5adb7c0ab4c0436ce3a18f
Emre <esokullu@gmail.com>
2019-08-14 14:29:04 -0700

    customer detail improvements

--- view/customer_detail.php
+++ view/customer_detail.php
@@ -10,13 +10,13 @@
         Email: <?= $this->e("$user->email") ?>
     </div>
     <div>
-        UUID:  <?=  $this->e($by->instances->first()->uuid) ?>
+        UUID:  <?=  $this->e($user->instances->first()->uuid) ?>
     </div>
     <div>
-                    Grou.ps Name: <?= $this->e($by->instances->first()->groups_name) ?>
+                    Grou.ps Name: <?= $this->e($user->instances->first()->groups_name) ?>
                 </div>
                 <div>
-                    Grou.ps Title: <?= $this->e($by->instances->first()->groups_title) ?>
+                    Grou.ps Title: <?= $this->e($user->instances->first()->groups_title) ?>
                 </div>
     <div>
         Website: <?= $this->e($user->instances->first()->site->url) ?>
@@ -29,13 +29,13 @@
                         substr(
                             md5(
                                 password_hash(
-                                    (string) $by->id, 
+                                    (string) $user->id, 
                                     PASSWORD_BCRYPT, 
                                     ["salt"=>"nuno gomes is a great soccer player"]
                                 )
                             )
                         , 3, 6), 
-                        (string) $by->id
+                        (string) $user->id
                     )
                     
                     ?>
@@ -44,18 +44,18 @@
                     Recreate Url: <?= 
                     
                     "https://gr.ps/generate?" . http_build_query([
-                        "name" => $by->instances->first()->groups_name,
-                        "title" => $by->instances->first()->groups_title,
-                        "public_id" =>$by->instances->first()->uuid,
-                        "theme"=>$by->instances->first()->theme,
-                        "text_color"=>($by->instances->first()->theme == "dark" ) ? "white" :  'rgb(63, 95, 127)',
-                        "background_color"=> ($by->instances->first()->theme == "dark" ) ? 'rgb(71, 71, 71)' :  "white",
-                        "primary_color"=>$by->instances->first()->color,
+                        "name" => $user->instances->first()->groups_name,
+                        "title" => $user->instances->first()->groups_title,
+                        "public_id" =>$user->instances->first()->uuid,
+                        "theme"=>$user->instances->first()->theme,
+                        "text_color"=>($user->instances->first()->theme == "dark" ) ? "white" :  'rgb(63, 95, 127)',
+                        "background_color"=> ($user->instances->first()->theme == "dark" ) ? 'rgb(71, 71, 71)' :  "white",
+                        "primary_color"=>$user->instances->first()->color,
                         "host"=>"https://accounts.groups2.com",
-                        "secret"=>md5($by->instances->first()->groups_name.":burasi mustur yolu yokustur"),
+                        "secret"=>md5($user->instances->first()->groups_name.":burasi mustur yolu yokustur"),
                         "regen"=>1,
-                        "git"=> "http://".$by->instances->first()->groups_name.":".substr(md5(password_hash($by->id, PASSWORD_BCRYPT, ["salt"=>"nuno gomes is a great soccer player"])), 3, 6) . (string) $by->id."@165.22.133.69/".$by->instances->first()->groups_name."/frontend",
-                        "description"=>$by->instances->first()->description,
+                        "git"=> "http://".$user->instances->first()->groups_name.":".substr(md5(password_hash($by->id, PASSWORD_BCRYPT, ["salt"=>"nuno gomes is a great soccer player"])), 3, 6) . (string) $by->id."@165.22.133.69/".$by->instances->first()->groups_name."/frontend",
+                        "description"=>$user->instances->first()->description,
                         "module_forum"=>0,
                         "module_groups"=>0
                 
