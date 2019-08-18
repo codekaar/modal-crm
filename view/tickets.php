eeac4e35e97c1cd2fe5dba22362d9f5dfb7b16da
Emre <esokullu@gmail.com>
2019-08-17 17:58:23 -0700

    bugfix

--- view/tickets.php
+++ view/tickets.php
@@ -1,4 +1,34 @@
-<?php $this->layout('layout/main.php', [ 'title' => 'Service Tickets' ]) ?>
+<?php 
+function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
+$this->layout('layout/main.php', [ 'title' => 'Service Tickets' ]) 
+?>
 
 <div class="container">
     <h1>Service Tickets</h1>
@@ -25,7 +55,7 @@
                     <td><?= $ticketTypeToText($ticket->type) ?></td>
                     <td><a href="mailto:<?= $this->e("{$ticket->byUser->email}") ?>"># <?= $this->e("{$ticket->byUser->first_name} {$ticket->byUser->last_name}") ?></a></td>
                     <!--<td><?= $ticket->assigneeUser ? $this->e("{$ticket->assigneeUser->first_name} {$ticket->assigneeUser->last_name}") : '' ?></td>-->
-                    <td><?= $ticket->open_date ?></td>
+                    <td><?= time_elapsed_string($ticket->open_date) ?></td>
                     <!--<td><?= $ticket->close_date ?></td>-->
                     <td><?= $ticketStatusToText($ticket->status) ?></td>
                     <td><?= $ticket->feedback ?></td>
