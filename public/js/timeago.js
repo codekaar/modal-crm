2aa791175b97e2e2693389ecafbece9b4c20d530
Emre <esokullu@gmail.com>
2019-08-17 18:07:01 -0700

    bugfix

--- public/js/timeago.js
+++ public/js/timeago.js
@@ -1,2 +1,232 @@
-   // https://github.com/hustcc/timeago.js/blob/master/gh-pages/timeago.min.js
-   !function(t,e){"object"==typeof exports&&"undefined"!=typeof module?e(exports):"function"==typeof define&&define.amd?define(["exports"],e):e(t.timeago={})}(this,function(t){"use strict";var f=[60,60,24,7,365/7/12,12],o=function(t){return parseInt(t)},n=function(t){return t instanceof Date?t:!isNaN(t)||/^\d+$/.test(t)?new Date(o(t)):(t=(t||"").trim().replace(/\.\d+/,"").replace(/-/,"/").replace(/-/,"/").replace(/(\d)T(\d)/,"$1 $2").replace(/Z/," UTC").replace(/([\+\-]\d\d)\:?(\d\d)/," $1$2"),new Date(t))},s=function(t,e){for(var n=0,r=t<0?1:0,a=t=Math.abs(t);f[n]<=t&&n<f.length;n++)t/=f[n];return(0===(n*=2)?9:1)<(t=o(t))&&(n+=1),e(t,n,a)[r].replace("%s",t)},d=function(t,e){return((e=e?n(e):new Date)-n(t))/1e3},r="second_minute_hour_day_week_month_year".split("_"),a="秒_分钟_小时_天_周_个月_年".split("_"),e=function(t,e){if(0===e)return["just now","right now"];var n=r[parseInt(e/2)];return 1<t&&(n+="s"),["".concat(t," ").concat(n," ago"),"in ".concat(t," ").concat(n)]},i={en_US:e,zh_CN:function(t,e){if(0===e)return["刚刚","片刻后"];var n=a[parseInt(e/2)];return["".concat(t," ").concat(n,"前"),"".concat(t," ").concat(n,"后")]}},c=function(t){return i[t]||e},l="timeago-tid",u=function(t,e){return t.getAttribute?t.getAttribute(e):t.attr?t.attr(e):void 0},p=function(t){return u(t,l)},_={},v=function(t){clearTimeout(t),delete _[t]},h=function t(e,n,r,a){v(p(e));var o=d(n,a);e.innerHTML=s(o,r);var i,c,u=setTimeout(function(){t(e,n,r,a)},1e3*function(t){for(var e=1,n=0,r=Math.abs(t);f[n]<=t&&n<f.length;n++)t/=f[n],e*=f[n];return r=(r%=e)?e-r:e,Math.ceil(r)}(o),2147483647);_[u]=0,c=u,(i=e).setAttribute?i.setAttribute(l,c):i.attr&&i.attr(l,c)};t.version="4.0.0-beta.2",t.format=function(t,e,n){var r=d(t,n);return s(r,c(e))},t.render=function(t,e,n){var r;void 0===t.length&&(t=[t]);for(var a=0;a<t.length;a++){r=t[a];var o=u(r,"datetime"),i=c(e);h(r,o,i,n)}return t},t.cancel=function(t){if(t)v(p(t));else for(var e in _)v(e)},t.register=function(t,e){i[t]=e},Object.defineProperty(t,"__esModule",{value:!0})});
+/**
+ * Timeago is a jQuery plugin that makes it easy to support automatically
+ * updating fuzzy timestamps (e.g. "4 minutes ago" or "about 1 day ago").
+ *
+ * @name timeago
+ * @version 1.6.7
+ * @requires jQuery >=1.5.0 <4.0
+ * @author Ryan McGeary
+ * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+ *
+ * For usage and examples, visit:
+ * http://timeago.yarp.com/
+ *
+ * Copyright (c) 2008-2019, Ryan McGeary (ryan -[at]- mcgeary [*dot*] org)
+ */
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['jquery'], factory);
+    } else if (typeof module === 'object' && typeof module.exports === 'object') {
+      factory(require('jquery'));
+    } else {
+      // Browser globals
+      factory(jQuery);
+    }
+  }(function ($) {
+    $.timeago = function(timestamp) {
+      if (timestamp instanceof Date) {
+        return inWords(timestamp);
+      } else if (typeof timestamp === "string") {
+        return inWords($.timeago.parse(timestamp));
+      } else if (typeof timestamp === "number") {
+        return inWords(new Date(timestamp));
+      } else {
+        return inWords($.timeago.datetime(timestamp));
+      }
+    };
+    var $t = $.timeago;
+  
+    $.extend($.timeago, {
+      settings: {
+        refreshMillis: 60000,
+        allowPast: true,
+        allowFuture: false,
+        localeTitle: false,
+        cutoff: 0,
+        autoDispose: true,
+        strings: {
+          prefixAgo: null,
+          prefixFromNow: null,
+          suffixAgo: "ago",
+          suffixFromNow: "from now",
+          inPast: 'any moment now',
+          seconds: "less than a minute",
+          minute: "about a minute",
+          minutes: "%d minutes",
+          hour: "about an hour",
+          hours: "about %d hours",
+          day: "a day",
+          days: "%d days",
+          month: "about a month",
+          months: "%d months",
+          year: "about a year",
+          years: "%d years",
+          wordSeparator: " ",
+          numbers: []
+        }
+      },
+  
+      inWords: function(distanceMillis) {
+        if (!this.settings.allowPast && ! this.settings.allowFuture) {
+            throw 'timeago allowPast and allowFuture settings can not both be set to false.';
+        }
+  
+        var $l = this.settings.strings;
+        var prefix = $l.prefixAgo;
+        var suffix = $l.suffixAgo;
+        if (this.settings.allowFuture) {
+          if (distanceMillis < 0) {
+            prefix = $l.prefixFromNow;
+            suffix = $l.suffixFromNow;
+          }
+        }
+  
+        if (!this.settings.allowPast && distanceMillis >= 0) {
+          return this.settings.strings.inPast;
+        }
+  
+        var seconds = Math.abs(distanceMillis) / 1000;
+        var minutes = seconds / 60;
+        var hours = minutes / 60;
+        var days = hours / 24;
+        var years = days / 365;
+  
+        function substitute(stringOrFunction, number) {
+          var string = $.isFunction(stringOrFunction) ? stringOrFunction(number, distanceMillis) : stringOrFunction;
+          var value = ($l.numbers && $l.numbers[number]) || number;
+          return string.replace(/%d/i, value);
+        }
+  
+        var words = seconds < 45 && substitute($l.seconds, Math.round(seconds)) ||
+          seconds < 90 && substitute($l.minute, 1) ||
+          minutes < 45 && substitute($l.minutes, Math.round(minutes)) ||
+          minutes < 90 && substitute($l.hour, 1) ||
+          hours < 24 && substitute($l.hours, Math.round(hours)) ||
+          hours < 42 && substitute($l.day, 1) ||
+          days < 30 && substitute($l.days, Math.round(days)) ||
+          days < 45 && substitute($l.month, 1) ||
+          days < 365 && substitute($l.months, Math.round(days / 30)) ||
+          years < 1.5 && substitute($l.year, 1) ||
+          substitute($l.years, Math.round(years));
+  
+        var separator = $l.wordSeparator || "";
+        if ($l.wordSeparator === undefined) { separator = " "; }
+        return $.trim([prefix, words, suffix].join(separator));
+      },
+  
+      parse: function(iso8601) {
+        var s = $.trim(iso8601);
+        s = s.replace(/\.\d+/,""); // remove milliseconds
+        s = s.replace(/-/,"/").replace(/-/,"/");
+        s = s.replace(/T/," ").replace(/Z/," UTC");
+        s = s.replace(/([\+\-]\d\d)\:?(\d\d)/," $1$2"); // -04:00 -> -0400
+        s = s.replace(/([\+\-]\d\d)$/," $100"); // +09 -> +0900
+        return new Date(s);
+      },
+      datetime: function(elem) {
+        var iso8601 = $t.isTime(elem) ? $(elem).attr("datetime") : $(elem).attr("title");
+        return $t.parse(iso8601);
+      },
+      isTime: function(elem) {
+        // jQuery's `is()` doesn't play well with HTML5 in IE
+        return $(elem).get(0).tagName.toLowerCase() === "time"; // $(elem).is("time");
+      }
+    });
+  
+    // functions that can be called via $(el).timeago('action')
+    // init is default when no action is given
+    // functions are called with context of a single element
+    var functions = {
+      init: function() {
+        functions.dispose.call(this);
+        var refresh_el = $.proxy(refresh, this);
+        refresh_el();
+        var $s = $t.settings;
+        if ($s.refreshMillis > 0) {
+          this._timeagoInterval = setInterval(refresh_el, $s.refreshMillis);
+        }
+      },
+      update: function(timestamp) {
+        var date = (timestamp instanceof Date) ? timestamp : $t.parse(timestamp);
+        $(this).data('timeago', { datetime: date });
+        if ($t.settings.localeTitle) {
+          $(this).attr("title", date.toLocaleString());
+        }
+        refresh.apply(this);
+      },
+      updateFromDOM: function() {
+        $(this).data('timeago', { datetime: $t.parse( $t.isTime(this) ? $(this).attr("datetime") : $(this).attr("title") ) });
+        refresh.apply(this);
+      },
+      dispose: function () {
+        if (this._timeagoInterval) {
+          window.clearInterval(this._timeagoInterval);
+          this._timeagoInterval = null;
+        }
+      }
+    };
+  
+    $.fn.timeago = function(action, options) {
+      var fn = action ? functions[action] : functions.init;
+      if (!fn) {
+        throw new Error("Unknown function name '"+ action +"' for timeago");
+      }
+      // each over objects here and call the requested function
+      this.each(function() {
+        fn.call(this, options);
+      });
+      return this;
+    };
+  
+    function refresh() {
+      var $s = $t.settings;
+  
+      //check if it's still visible
+      if ($s.autoDispose && !$.contains(document.documentElement,this)) {
+        //stop if it has been removed
+        $(this).timeago("dispose");
+        return this;
+      }
+  
+      var data = prepareData(this);
+  
+      if (!isNaN(data.datetime)) {
+        if ( $s.cutoff === 0 || Math.abs(distance(data.datetime)) < $s.cutoff) {
+          $(this).text(inWords(data.datetime));
+        } else {
+          if ($(this).attr('title').length > 0) {
+              $(this).text($(this).attr('title'));
+          }
+        }
+      }
+      return this;
+    }
+  
+    function prepareData(element) {
+      element = $(element);
+      if (!element.data("timeago")) {
+        element.data("timeago", { datetime: $t.datetime(element) });
+        var text = $.trim(element.text());
+        if ($t.settings.localeTitle) {
+          element.attr("title", element.data('timeago').datetime.toLocaleString());
+        } else if (text.length > 0 && !($t.isTime(element) && element.attr("title"))) {
+          element.attr("title", text);
+        }
+      }
+      return element.data("timeago");
+    }
+  
+    function inWords(date) {
+      return $t.inWords(distance(date));
+    }
+  
+    function distance(date) {
+      return (new Date().getTime() - date.getTime());
+    }
+  
+    // fix for IE6 suckage
+    document.createElement("abbr");
+    document.createElement("time");
+  }));
